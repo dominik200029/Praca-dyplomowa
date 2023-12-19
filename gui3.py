3fd1c67d7169cb298dea67528d9713ba579f33dc
dominik200029 <dominik.duchnik@onet.pl>
2023-12-19 20:24:18 +0100

    Commit

--- gui3.py
+++ gui3.py
@@ -36,39 +36,51 @@ class Canvas:
         self.ax.clear()
 
 
-class SamplingHandler:
-    def __init__(self, samples_number, sampling_frequency, time_step):
-        self.samples_number = samples_number
-        self.sampling_frequency = sampling_frequency
-        self.time_step = time_step
+def print_error(message):
+    msg = QMessageBox()
+    msg.setWindowTitle("Error")
+    msg.setText(message)
+    msg.exec_()
+
+
+class AxisHandler:
+    def __init__(self, data):
+        self.samples_number = data['samples_number']
+        self.sampling_frequency = data['sampling_frequency']
+        self.time_step = data['time_step']
 
     def generate_time_axis(self):
         time_axis = np.arange(0, (self.samples_number - 1) / self.sampling_frequency, self.time_step)
         return time_axis
 
+    def generate_frequency_axis(self, signal):
+        fft_x_axis = fftfreq(len(signal), 1 / self.sampling_frequency)
+        return fft_x_axis
+
 
 class Signal:
-    def __init__(self, amplitude, frequency, phase, samples_number, sampling_frequency, time_step):
+    def __init__(self, amplitude, frequency, phase):
         self.amplitude = amplitude
         self.frequency = frequency
         self.phase = phase
 
-    #    self.samples_number = samples_number
-    #    self.sampling_frequency = sampling_frequency
-    #    self.time_step = time_step
-    #    self.time_axis = []
-
-    def generate_sine_wave(self, time):
+    def calculate_sine_wave(self, time):
         signal = self.amplitude * np.sin(2 * np.pi * self.frequency * time + self.phase)
         return signal
 
 
 class FFTAnalyzer:
-    def __init__(self, signal: Signal):
+    def __init__(self, signal):
         self.signal = signal
 
-    def calculate_fft(self):
-        pass
+    def calculate_fft(self, data):
+        axis = AxisHandler(data)
+        fft_x_axis = axis.generate_frequency_axis(self.signal)
+        fft_data = fft(self.signal)
+        return fft_x_axis, fft_data
+
+    def calculate_ifft(self):
+        return ifft(self.signal)
 
 
 class View(QMainWindow):
@@ -79,7 +91,7 @@ class View(QMainWindow):
 
         self.canvas_dict = {
             'signal': Canvas(self.layout1),
-            'dft': Canvas(self.layout2),
+            'fft': Canvas(self.layout2),
             'ifft': Canvas(self.layout3),
             'dct': Canvas(self.layout4),
             'idct': Canvas(self.layout5)
@@ -97,52 +109,79 @@ class View(QMainWindow):
 
 
 class Model:
-    def generate_signal(self, amplitude, frequency, phase, samples_number, sampling_frequency, time_step):
-        signal = Signal(amplitude, frequency, phase, samples_number, sampling_frequency, time_step)
-        sampling_parameters = SamplingHandler(samples_number, sampling_frequency, time_step)
-        time_axis = sampling_parameters.generate_time_axis()
-        y_data = signal.generate_sine_wave(time_axis)
-        return y_data, time_axis
+    def __init__(self):
+        self.signals_amount = 0
+        self.signals = []
 
+    def add_signal(self, data):
+        self.signals.append(Signal(data['amplitude'][self.signals_amount], data['frequency'][self.signals_amount],
+                                   data['phase'][self.signals_amount]))
+        self.signals_amount += 1
 
-def print_error(message):
-    msg = QMessageBox()
-    msg.setWindowTitle("Error")
-    msg.setText(message)
-    msg.exec_()
+    def generate_wave(self, data):
+        axis = AxisHandler(data)
+        time_axis = axis.generate_time_axis()
+        wave = np.zeros(len(time_axis))
+
+        for signal in self.signals:
+            wave += signal.amplitude * np.sin(2 * np.pi * signal.frequency * time_axis + signal.phase)
+
+        return time_axis, wave
+
+    def generate_fft(self, axis_data):
+        wave = self.generate_wave(axis_data)[1]
+        fft_analyzer = FFTAnalyzer(wave)
+        fft_axis, fft_data = fft_analyzer.calculate_fft(axis_data)
+        return fft_axis, fft_data
+
+    def generate_ifft(self, fft_function):
+        fft_analyzer = FFTAnalyzer(fft_function)
+        ifft_data = fft_analyzer.calculate_ifft()
+        return np.real(ifft_data)
 
 
 class Controller:
     def __init__(self, model, view):
         self.model = model
         self.view = view
-        self.y_data = np.array([])  # Initialize as an empty NumPy array
-        self.time_axis = np.array([])  # Initialize as an empty NumPy array
+        self.amplitude = []
+        self.frequency = []
+        self.phase = []
 
-    def collect_data(self):
+    def collect_signal_data(self):
         amplitude_text = self.view.amplitude_edit.text()
         frequency_text = self.view.frequency_edit.text()
         phase_text = self.view.phase_edit.text()
+
+        if not (amplitude_text and frequency_text and phase_text):
+            print_error("Wprowadz wszystkie dane")
+            return None
+
+        self.amplitude.append(float(amplitude_text))
+        self.frequency.append(float(frequency_text))
+        self.phase.append(float(phase_text))
+
+        data = {
+            'amplitude': self.amplitude,
+            'frequency': self.frequency,
+            'phase': self.phase
+        }
+        return data
+
+    def collect_axis_data(self):
         time_step_text = self.view.time_step_edit.text()
         sampling_frequency_text = self.view.sampling_fr_edit.text()
         samples_number_text = self.view.samples_number_edit.text()
 
-        if not (amplitude_text and frequency_text and
-                sampling_frequency_text and phase_text and samples_number_text):
-            print_error("Wprowadź wszystkie dane")
+        if not (time_step_text and sampling_frequency_text and samples_number_text):
+            print_error("Wprowadz wszystkie dane")
             return None
 
-        amplitude = float(amplitude_text)
-        frequency = float(frequency_text)
-        phase = float(phase_text)
         time_step = float(time_step_text)
         sampling_frequency = float(sampling_frequency_text)
         samples_number = float(samples_number_text)
 
         data = {
-            'amplitude': amplitude,
-            'frequency': frequency,
-            'phase': phase,
             'time_step': time_step,
             'sampling_frequency': sampling_frequency,
             'samples_number': samples_number
@@ -150,28 +189,21 @@ class Controller:
         return data
 
     def handle_update_button(self):
-        # clean all canvases
         self.view.clear_canvas()
-
-        # Update signal's canvas
-        self.view.update_canvas('signal', self.time_axis, self.y_data,
-                                'Sum of Signals', 'plot', 'Time', 'Amplitude')
+        data = self.collect_axis_data()
+        time_axis, wave = self.model.generate_wave(data)
+        self.view.update_canvas('signal', time_axis, wave, 'Original signal', 'plot', 'Time',
+                                'Amplitude')
+        fft_x_data, fft_data = self.model.generate_fft(data)
+        self.view.update_canvas('fft', fft_x_data, abs(fft_data), 'abs(FFT)', 'stem', 'Frequency',
+                                'Magnitude')
+        ifft_data = self.model.generate_ifft(fft_data)
+        self.view.update_canvas('ifft', time_axis, np.real(ifft_data), 'Inverse FFT', 'plot', 'Time',
+                                'Amplitude')
 
     def handle_add_button(self):
-        data = self.collect_data()
-        generated_wave, time_axis = self.model.generate_signal(data['amplitude'], data['frequency'], data['phase'],
-                                                               data['samples_number'], data['sampling_frequency'],
-                                                               data['time_step'])
-
-        if not self.y_data.size:
-            self.y_data = generated_wave
-            self.time_axis = time_axis
-        else:
-            # Ensure the dimensions match before concatenating
-            if len(self.y_data) == len(generated_wave):
-                self.y_data = np.add(self.y_data, generated_wave)
-            else:
-                print_error("Error: Dimensions mismatch. Cannot add signals.")
+        data = self.collect_signal_data()
+        self.model.add_signal(data)
 
 
 class App(QApplication):
